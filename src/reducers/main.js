57dfadb5e6a8470cbc32981c446c1a21d4b10837
marina <marina_bakum@mail.ru>
2018-01-31 05:21:41 +0200

    defaultState

--- src/reducers/main.js
+++ src/reducers/main.js
@@ -1,19 +1,23 @@
 import update from 'immutability-helper'
 
 let defaultState = {
-  name: 'Danya'
+    name: '',
+    email: '',
+    phone: '',
+    address: '',
+    postcode: '',
+    dateBirth: ''
 }
 
 const main = (state = defaultState, action) => {
-  switch (action.type) {
-    case 'CHANGE_STATE_PROPS':
-      console.log('data in reducer', action.state)
-      return update(state, {
-        [action.state.prop]: {$set: action.state.value}
-      })
-    default:
-      return state
-  }
+    switch (action.type) {
+        case 'CHANGE_STATE_PROPS':
+            return update(state, {
+                [action.state.prop]: {$set: action.state.value}
+            })
+        default:
+            return state
+    }
 }
 
 export default main
