f63c7d5a90706dc71a763b84a2efbefccf0b2a06
marina <marina_bakum@mail.ru>
2018-01-31 05:17:21 +0200

    change app

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-React application using redux with persistent storage (should work on refresh) and validations. Fields: name (required), email (required), phone (optional), address (optional), postcode (required), date of birth (required, datepicker). Show hints if field is invalid (popovers).
+React application using redux with persistent storage (should work on refresh) and validations.
+Fields: name (required), email (required), phone (optional), address (optional), postcode (required), date of birth (required, datepicker). 
+Show hints if field is invalid (popovers).
+
 To start the project, first install all packages with next command
 
     npm install
--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import TestComponent from './components/test-component/testComponentContainer'
-import SecondComponent from './components/secondComponent/secondComponentContainer'
+import Form from './components/form/formContainer'
+import View from './components/view/viewContainer'
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
         <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to React</h1>
+          <h1 className='App-title'>React redux form</h1>
         </header>
-        <TestComponent testName='Kristina' />
-        <SecondComponent />
+        <Form />
+        <View />
       </div>
     )
   }
